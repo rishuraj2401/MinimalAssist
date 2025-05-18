c4bc73622638bf38d1b491c5cce1e5f29dab09b5
Rishu Raj <rishuraj2401sinha@gmail.com>
2025-05-18 23:33:37 +0530

    frontend-url

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -9,7 +9,7 @@ import { config } from "dotenv";
 config();
 const app = express();
 // Middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://minimal-assist.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev")); // for development
